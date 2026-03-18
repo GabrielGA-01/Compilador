93f6cfffc4c4da557d2201da3624cbbc07160379
Gabriel <gabrielgarcia6508@gmail.com>
2026-03-18 17:57:52 -0300

    Tentativa de executar o tiny

--- Tiny Linux/yacc/globals.h
+++ Tiny Linux/yacc/globals.h
@@ -27,7 +27,7 @@
 #ifndef YYPARSER
 
 /* the name of the following file may change */
-#include "y.tab.h"
+#include "../tiny.tab.h"
 
 /* ENDFILE is implicitly defined by Yacc/Bison,
  * and not included in the tab.h file
