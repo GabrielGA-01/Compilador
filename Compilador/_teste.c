63189982f989c2fd54872ce0dc15ee9cd83ce735
Gabriel <gabrielgarcia6508@gmail.com>
2026-06-14 21:34:04 -0300

    fix: changed R0 to R11 in _test.c

--- Compilador/_teste.c
+++ Compilador/_teste.c
@@ -34,18 +34,18 @@ Quad* makeNewQuadTest(QuadOp op, Address a1, Address a2, Address a3) {
 
 void main(){
     // --- Inicialização ---
-    makeNewQuadTest(OP_MOVI, *createRegisterAddr(0), createNumericAddr(10), createEmptyAddr());
+    makeNewQuadTest(OP_MOVI, *createRegisterAddr(11), createNumericAddr(10), createEmptyAddr());
     makeNewQuadTest(OP_MOVI, *createRegisterAddr(1), createNumericAddr(20), createEmptyAddr());
     makeNewQuadTest(OP_MOVI, *createRegisterAddr(2), createNumericAddr(20), createEmptyAddr());
 
     // --- Aritmética ---
-    makeNewQuadTest(OP_ADDR, *createRegisterAddr(3), *createRegisterAddr(1), *createRegisterAddr(0));
+    makeNewQuadTest(OP_ADDR, *createRegisterAddr(3), *createRegisterAddr(1), *createRegisterAddr(11));
     makeNewQuadTest(OP_ADDI, *createRegisterAddr(4), *createRegisterAddr(1), createNumericAddr(10));
-    makeNewQuadTest(OP_SUBR, *createRegisterAddr(5), *createRegisterAddr(1), *createRegisterAddr(0));
+    makeNewQuadTest(OP_SUBR, *createRegisterAddr(5), *createRegisterAddr(1), *createRegisterAddr(11));
     makeNewQuadTest(OP_SUBI, *createRegisterAddr(6), *createRegisterAddr(1), createNumericAddr(10));
-    makeNewQuadTest(OP_MULR, *createRegisterAddr(7), *createRegisterAddr(1), *createRegisterAddr(0));
+    makeNewQuadTest(OP_MULR, *createRegisterAddr(7), *createRegisterAddr(1), *createRegisterAddr(11));
     makeNewQuadTest(OP_MULI, *createRegisterAddr(8), *createRegisterAddr(1), createNumericAddr(10));
-    makeNewQuadTest(OP_DIVR, *createRegisterAddr(9), *createRegisterAddr(1), *createRegisterAddr(0));
+    makeNewQuadTest(OP_DIVR, *createRegisterAddr(9), *createRegisterAddr(1), *createRegisterAddr(11));
     makeNewQuadTest(OP_DIVI, *createRegisterAddr(10), *createRegisterAddr(1), createNumericAddr(10));
 
         // --- BEQ ---
@@ -56,7 +56,7 @@ void main(){
 
     Address l1 = *createLabelAddr();
     Address l2 = *createLabelAddr();
-    makeNewQuadTest(OP_BEQ, *createRegisterAddr(0), *createRegisterAddr(1), l1);
+    makeNewQuadTest(OP_BEQ, *createRegisterAddr(11), *createRegisterAddr(1), l1);
     makeNewQuadTest(OP_JUMP, l2, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l1, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
@@ -64,7 +64,7 @@ void main(){
 
     // --- BNE ---
     Address l3 = *createLabelAddr();
-    makeNewQuadTest(OP_BNE, *createRegisterAddr(0), *createRegisterAddr(1), l3);
+    makeNewQuadTest(OP_BNE, *createRegisterAddr(11), *createRegisterAddr(1), l3);
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l3, createEmptyAddr(), createEmptyAddr());
 
@@ -78,13 +78,13 @@ void main(){
 
     // --- BLT ---
     Address l6 = *createLabelAddr();
-    makeNewQuadTest(OP_BLT, *createRegisterAddr(0), *createRegisterAddr(1), l6);
+    makeNewQuadTest(OP_BLT, *createRegisterAddr(11), *createRegisterAddr(1), l6);
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l6, createEmptyAddr(), createEmptyAddr());
 
     Address l7 = *createLabelAddr();
     Address l8 = *createLabelAddr();
-    makeNewQuadTest(OP_BLT, *createRegisterAddr(1), *createRegisterAddr(0), l7);
+    makeNewQuadTest(OP_BLT, *createRegisterAddr(1), *createRegisterAddr(11), l7);
     makeNewQuadTest(OP_JUMP, l8, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l7, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
@@ -92,13 +92,13 @@ void main(){
 
     // --- BGT ---
     Address l9 = *createLabelAddr();
-    makeNewQuadTest(OP_BGT, *createRegisterAddr(1), *createRegisterAddr(0), l9);
+    makeNewQuadTest(OP_BGT, *createRegisterAddr(1), *createRegisterAddr(11), l9);
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l9, createEmptyAddr(), createEmptyAddr());
 
     Address l10 = *createLabelAddr();
     Address l11 = *createLabelAddr();
-    makeNewQuadTest(OP_BGT, *createRegisterAddr(0), *createRegisterAddr(1), l10);
+    makeNewQuadTest(OP_BGT, *createRegisterAddr(11), *createRegisterAddr(1), l10);
     makeNewQuadTest(OP_JUMP, l11, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l10, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
@@ -106,13 +106,13 @@ void main(){
 
     // --- BLE ---
     Address l12 = *createLabelAddr();
-    makeNewQuadTest(OP_BLE, *createRegisterAddr(0), *createRegisterAddr(1), l12);
+    makeNewQuadTest(OP_BLE, *createRegisterAddr(11), *createRegisterAddr(1), l12);
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l12, createEmptyAddr(), createEmptyAddr());
 
     Address l13 = *createLabelAddr();
     Address l14 = *createLabelAddr();
-    makeNewQuadTest(OP_BLE, *createRegisterAddr(1), *createRegisterAddr(0), l13);
+    makeNewQuadTest(OP_BLE, *createRegisterAddr(1), *createRegisterAddr(11), l13);
     makeNewQuadTest(OP_JUMP, l14, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l13, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
@@ -120,13 +120,13 @@ void main(){
 
     // --- BGE ---
     Address l15 = *createLabelAddr();
-    makeNewQuadTest(OP_BGE, *createRegisterAddr(1), *createRegisterAddr(0), l15);
+    makeNewQuadTest(OP_BGE, *createRegisterAddr(1), *createRegisterAddr(11), l15);
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l15, createEmptyAddr(), createEmptyAddr());
 
     Address l16 = *createLabelAddr();
     Address l17 = *createLabelAddr();
-    makeNewQuadTest(OP_BGE, *createRegisterAddr(0), *createRegisterAddr(1), l16);
+    makeNewQuadTest(OP_BGE, *createRegisterAddr(11), *createRegisterAddr(1), l16);
     makeNewQuadTest(OP_JUMP, l17, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l16, createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
@@ -139,7 +139,7 @@ void main(){
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
     makeNewQuadTest(OP_LABEL, l18, createEmptyAddr(), createEmptyAddr());
 
-    makeNewQuadTest(OP_MOVR, *createRegisterAddr(4), *createRegisterAddr(0), createEmptyAddr());
+    makeNewQuadTest(OP_MOVR, *createRegisterAddr(4), *createRegisterAddr(11), createEmptyAddr());
     makeNewQuadTest(OP_STOREDI, *createRegisterAddr(4), *createRegisterAddr(1), createNumericAddr(5));
     makeNewQuadTest(OP_LOADDI, *createRegisterAddr(5), *createRegisterAddr(4), createNumericAddr(5));
     makeNewQuadTest(OP_HALT, createEmptyAddr(), createEmptyAddr(), createEmptyAddr());
