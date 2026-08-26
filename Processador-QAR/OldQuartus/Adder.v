7cb009ccd45b4a3c802c2977c3adce49b786ecbd
Gabriel <gabrielgarcia6508@gmail.com>
2026-08-26 20:11:17 -0300

    chore: removed Quartus files

--- Processador-QAR/OldQuartus/Adder.v
+++
@@ -1,9 +0,0 @@
-module Adder (
-input [31:0] entrada1,
-input [31:0] entrada2,
-output [31:0] saida
-);
-
-assign saida = entrada1 + entrada2;
-
-endmodule 
--- Processador-QAR/OldQuartus/Sub1.v
+++
@@ -1,8 +0,0 @@
-module Sub1 (
-input [31:0] entrada,
-output [31:0] saida
-);
-
-assign saida = entrada - 1;
-
-endmodule 
